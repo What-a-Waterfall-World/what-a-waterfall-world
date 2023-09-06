6843dae5ad64c9a461e345369ef8bb84ac3c248d
Kim Idsinga <kimidsinga@gmail.com>
2023-09-06 18:58:43 +0200

    Implemented Bootstrap form validator

--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("what-a-waterfall-world JS imported successfully!");
 });
+
+// Form validation help for create/edit & register/login forms
+  (function () {
+  "use strict";
+
+  const forms = document.querySelectorAll(".needs-validation");
+
+  Array.from(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
