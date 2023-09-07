69735f4faa2225ed88c47ebe13742c78ad1a483e
Kim Idsinga <kimidsinga@gmail.com>
2023-09-07 22:30:54 +0200

    Added more waterfalls to seeds file

--- bin/seeds.js
+++ bin/seeds.js
@@ -83,6 +83,136 @@ const waterfalls = [
   },
   userDetails: "64fa1e0ff69c01f674daf554"
 },
+    {
+    name: `Manafossen Falls`,
+    imageUrl: "../images/Manafossen-Falls.jpeg",
+    postalCode: "6330",
+    city: `Verma`,
+    country: `Norway`,
+    transportation: "Bus, Car and Boat",
+    location: {
+      type: "Point",
+      coordinates: [62.44302261788418, 7.895236784353601]
+  },
+  userDetails: "64fa1e0ff69c01f674daf554"
+},
+    {
+    name: `Gavarnie Falls`,
+    imageUrl: "../images/Gavarnie-falls.jpeg",
+    postalCode: "65120",
+    city: `Gavarnie-Gèdre`,
+    country: `France`,
+    transportation: "Bus, Car and Boat",
+    location: {
+      type: "Point",
+      coordinates: [42.694320238593306, -0.004579831760847311]
+  },
+  userDetails: "64fa1e0ff69c01f674daf554"
+},
+    {
+    name: `Dettifoss`,
+    imageUrl: "../images/Dettifoss.jpeg",
+    postalCode: "671",
+    city: `n/a`,
+    country: `Iceland`,
+    transportation: "Bus, Car and Boat",
+    location: {
+      type: "Point",
+      coordinates: [65.8150262297608, -16.384146840537067]
+  },
+  userDetails: "64fa1e0ff69c01f674daf554"
+},
+    {
+    name: `Rhine Falls`,
+    imageUrl: "../images/Rhine-Falls.jpeg",
+    postalCode: "8447",
+    city: `Laufen-Uhwiesen`,
+    country: `Swiss`,
+    transportation: "Bus, Car and Boat",
+    location: {
+      type: "Point",
+      coordinates: [47.67880284572023, 8.615365319748337]
+  },
+  userDetails: "64fa1e0ff69c01f674daf554"
+},
+    {
+    name: `Vøringfossen`,
+    imageUrl: "../images/Vøringfosse.jpeg",
+    postalCode: "5785",
+    city: `Vøringfoss`,
+    country: `Norway`,
+    transportation: "Bus, Car and Boat",
+    location: {
+      type: "Point",
+      coordinates: [60.42692107612453, 7.251637463223289]
+  },
+  userDetails: "64fa1e0ff69c01f674daf554"
+},
+    {
+    name: `Gullfoss Falls`,
+    imageUrl: "../images/Gullfoss.jpeg",
+    postalCode: "846",
+    city: `n/a`,
+    country: `Iceland`,
+    transportation: "Bus, Car and Boat",
+    location: {
+      type: "Point",
+      coordinates: [64.32716093858001, -20.11991956509619]
+  },
+  userDetails: "64fa1e0ff69c01f674daf554"
+},
+    {
+    name: `Torc Waterfall`,
+    imageUrl: "../images/Torc.jpeg",
+    postalCode: "",
+    city: `Torc, Killarney, Co. Kerry`,
+    country: `Ireland`,
+    transportation: "Bus, Car and Boat",
+    location: {
+      type: "Point",
+      coordinates: [52.00236429751708, -9.506418344334463]
+  },
+  userDetails: "64fa1e0ff69c01f674daf554"
+},
+    {
+    name: `Catrigg Force`,
+    imageUrl: "../images/Catrigg.jpeg",
+    postalCode: "BD24 9PZ",
+    city: `Settle`,
+    country: `The United Kingdom`,
+    transportation: "Bus, Car and Boat",
+    location: {
+      type: "Point",
+      coordinates: [54.10011645364024, -2.2563030040124596]
+  },
+  userDetails: "64fa1e0ff69c01f674daf554"
+},
+    {
+    name: `Terme di Saturnia`,
+    imageUrl: "../images/Terme.jpeg",
+    postalCode: "58014",
+    city: `Grosseto`,
+    country: `Italy`,
+    transportation: "Bus, Car and Boat",
+    location: {
+      type: "Point",
+      coordinates: [42.65849068195972, 11.515391823375833]
+  },
+  userDetails: "64fa1e0ff69c01f674daf554"
+},
+    {
+    name: `Sgwd Yr Eira Waterfall`,
+    imageUrl: "../images/SgwdYr.jpeg",
+    postalCode: "",
+    city: `River Hepste`,
+    country: `The United Kingdom`,
+    transportation: "Bus, Car and Boat",
+    location: {
+      type: "Point",
+      coordinates: [51.77844803093481, -3.553841071125484]
+  },
+  userDetails: "64fa1e0ff69c01f674daf554"
+},
 ]
 
 
